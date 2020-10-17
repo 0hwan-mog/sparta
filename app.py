95597ec6ca4726e3fc377dcdbbc3e12dcb2f9326
0hwan-mog <kyhwan91@gmail.com>
2020-10-17 16:52:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost',27017)
+
 db = client.dbhomework
 
 ## HTML 화면 보여주기
